ce7d243260f1a5f56e949463fa804289300a6bf0
UJIE-IC <yujie.yao@nuaa.edu.cn>
2026-09-16 19:41:13 +0800

    top&initial value

---
+++ sqrt_top.v
@@ -0,0 +1,22 @@
+module sqrt_top (
+    input clk,
+    input rst_n,
+    input start,
+    input [15:0] x_in,
+
+    output busy,
+    output done,
+    output [8:0] sqrt_out
+);
+
+sqrt_core u_sqrt_core (
+    .clk      (clk),
+    .rst_n    (rst_n),
+    .start    (start),
+    .x_in     (x_in),
+    .busy     (busy),
+    .done     (done),
+    .sqrt_out (sqrt_out)
+);
+
+endmodule
